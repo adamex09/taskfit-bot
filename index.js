8eae0d081e366bcbda58bc491aa1d453700f8b71
Adam Hornyak <hello@adamhornyak.com>
2016-11-08 22:55:03 +0100

    getForecast

--- index.js
+++ index.js
@@ -132,9 +132,14 @@ const actions = {
   // See https://wit.ai/docs/quickstart
   getForecast({context, entities}) {
     return new Promise(function(resolve, reject) {
-      // Here should go the api call, e.g.:
-      // context.forecast = apiCall(context.loc)
-      context.forecast = 'sunny';
+      var location = firstEntityValue(entities, "location")
+      if (location) {
+        context.forecast = 'sunny in ' + location; // we should call a weather API here
+        delete context.missingLocation;
+      } else {
+        context.missingLocation = true;
+        delete context.forecast;
+      }
       return resolve(context);
     });
   },
