5e7834b60bc6d9f9b9d69c6756d6383b1023c252
Adam Hornyak <hello@adamhornyak.com>
2016-11-08 22:02:57 +0100

    new consol log

--- index.js
+++ index.js
@@ -218,7 +218,7 @@ app.post('/webhook', (req, res) => {
 
               // Updating the user's current session state
               sessions[sessionId].context = context;
-              console.log('Context' + sessions[sessionId].context);
+              console.log('Context' + JSON.stringify(context));
             })
             .catch((err) => {
               console.error('Oops! Got an error from Wit: ', err.stack || err);
